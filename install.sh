8d63885f3200dc837dfb6ac873eb4b35b25a9f1d
Gordon <dqna64@gmail.com>
2026-05-05 15:09:04 +1000

    install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This install.sh script is empty for now, not needed bc all my dotfiles
+setup is currently done in `.zshrc` on zsh startup. And since this is a
+bare git repo, `.zshrc` can be cloned directly into `~`.
+
